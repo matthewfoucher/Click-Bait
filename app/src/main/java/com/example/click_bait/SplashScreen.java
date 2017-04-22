f87f889839470a8229540f9bd4c52558f9755384
matthewfoucher <matthewfoucher@gmail.com>
2017-04-22 14:15:28 -0700

    Added button onClick, reformatted splash screen

--- app/src/main/java/com/example/click_bait/SplashScreen.java
+++ app/src/main/java/com/example/click_bait/SplashScreen.java
@@ -1,7 +1,10 @@
 package com.example.click_bait;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
+
+    public void sellSoul(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "You have sold your soul!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
